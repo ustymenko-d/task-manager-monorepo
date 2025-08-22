e5d9c124b100f2155e7ecd65aa2874dfa889652c
ustymenko-d <ustymenko.denys@gmail.com>
2025-08-22 17:21:54 +0300

    ci/cd(api): test trigget

--- apps/api/src/app.controller.spec.ts
+++ apps/api/src/app.controller.spec.ts
@@ -16,8 +16,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Hello from server!"', () => {
+      expect(appController.getHello()).toBe('Hello from server!');
     });
   });
 });
