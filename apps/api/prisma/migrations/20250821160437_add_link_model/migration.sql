186dcd25b28efcfac72c900d4e060e287f28c150
ustymenko-d <ustymenko.denys@gmail.com>
2025-08-27 12:38:13 +0300

    feat: connect old db

--- apps/api/prisma/migrations/20250821160437_add_link_model/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "public"."Link" (
-    "id" TEXT NOT NULL,
-    "url" TEXT NOT NULL,
-    "title" TEXT NOT NULL,
-    "description" TEXT NOT NULL,
-
-    CONSTRAINT "Link_pkey" PRIMARY KEY ("id")
-);
